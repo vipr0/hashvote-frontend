226b99946f3cd6b78d7439d2195392ad4e64dee3
Vitaliy Protsyk <eqellez@gmail.com>
2020-05-05 16:02:59 +0300

    Changed VotingResult layout

--- src/components/VotingDetails/VotingResult.js
+++ src/components/VotingDetails/VotingResult.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Typography, Progress, Card } from "antd";
 
-const { Title } = Typography;
+const { Text } = Typography;
 
 const VotingResult = ({ loading, result, allVotes }) => {
   return (
-    <Card loading={loading}>
-      <Title level={3}>Candidates</Title>
+    <Card loading={loading} title="Candidates">
       {result &&
         result.map((candidate, i) => (
-          <div key={i}>
-            <Title level={4}>{`${i + 1}. ${candidate.name}`}</Title>
+          <div key={i} style={{ marginBottom: 20 }}>
+            <Text strong> {`${i + 1}. ${candidate.name}`} </Text>
+
             <Progress
               percent={Math.round((candidate.votesNum / allVotes) * 100)}
             />
