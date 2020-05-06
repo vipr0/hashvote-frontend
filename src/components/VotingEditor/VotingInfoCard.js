ba574be9e2d25d0d0c384bf029beb26babe1867e
Vitaliy Protsyk <eqellez@gmail.com>
2020-05-06 18:53:36 +0300

    Added ID and name of creator to VotingInfoCard

--- src/components/VotingEditor/VotingInfoCard.js
+++ src/components/VotingEditor/VotingInfoCard.js
@@ -1,21 +1,25 @@
 import React from "react";
 import { Descriptions, Card } from "antd";
 import moment from "moment";
+import { Link } from "react-router-dom";
 
 const VotingInfoCard = ({ voting }) => {
   return (
     <Card title="Information">
       <Descriptions className="voting-info" column={1}>
+        <Descriptions.Item label="ID">{voting._id}</Descriptions.Item>
         <Descriptions.Item label="Created At">
           {moment(voting.createdAt).format("MMMM Do YYYY, HH:mm:ss")}
         </Descriptions.Item>
         <Descriptions.Item label="Created By">
-          {voting.createdBy}
+          <Link to={`/admin/users/${voting.createdBy._id}`}>
+            {voting.createdBy.name}
+          </Link>
         </Descriptions.Item>
         <Descriptions.Item label="End Time">
           {moment(voting.endTime).format("MMMM Do YYYY, HH:mm:ss")}
         </Descriptions.Item>
-        <Descriptions.Item label="Voting ID">
+        <Descriptions.Item label="Contract ID">
           {voting.votingId}
         </Descriptions.Item>
         <Descriptions.Item label="Creation Tx">{voting.tx}</Descriptions.Item>
