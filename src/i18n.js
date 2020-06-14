8162c680adac52605c48cdda7fd03bc47cdabf91
Vitaliy Protsyk <eqellez@gmail.com>
2020-06-14 18:19:31 +0300

    Added ukrainian translation and language  detector

--- src/i18n.js
+++ src/i18n.js
@@ -1,8 +1,10 @@
 import i18n from "i18next";
+import detector from "i18next-browser-languagedetector";
 import { reactI18nextModule } from "react-i18next";
 
 import translationEn from "./locales/en/translation.json";
 import translationRu from "./locales/ru/translation.json";
+import translationUa from "./locales/ua/translation.json";
 
 const resources = {
   en: {
@@ -11,13 +13,20 @@ const resources = {
   ru: {
     translation: translationRu,
   },
+  ua: {
+    translation: translationUa,
+  },
 };
 
-i18n.use(reactI18nextModule).init({
-  resources,
-  lng: "ru",
-  keySeparator: false,
-  interpolation: { escapeValue: false },
-});
+i18n
+  .use(detector)
+  .use(reactI18nextModule)
+  .init({
+    resources,
+    lng: "en",
+    fallbackLng: "ua",
+    keySeparator: false,
+    interpolation: { escapeValue: false },
+  });
 
 export default i18n;
